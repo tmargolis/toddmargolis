956963cef5eb54e392d973beb820926dc871e745
Todd Margolis <todd.margolis@qlik.com>
2026-02-02 06:38:59 -0600

    Update projects.ts
    Qlik

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -630,9 +630,9 @@ export const projects: Project[] = [
     subCategory: ['R&D Leadership', 'Display & Interaction Systems'],
     year: '2014-present',
     medium: 'Sr. Director of Applied Research & Emerging Technologies',
-    heroImage: '/images/placeholder.png',
+    heroImage: '/images/Qlik_Logo_No_Trademark_2_Color_Positive_RGB.png',
     description: 'Lead a team of ML engineers accelerating our long-term roadmap, defining strategy and developing new capabilities leveraging emerging media platforms. My deep empathy for customer and user needs paired with my passion for innovation drives my creative, yet data-driven approach to prioritization. I have extensive prototyping and presentation experience and enjoy providing thought leadership in executive briefings, conference keynotes and academic symposiums globally. At Qlik, I have previously held roles as Director of Product Management as well as a Sr. Solution Architect.',
-    images: ['/images/Qlik_Logo_No_Trademark_2_Color_Positive_RGB.png']
+    images: ['/images/placeholder.png']
   },
   // 2. University of California, San Diego (Qualcomm Institute)
   {
