ed5d2974d5ee2a400f6bf7f8f56b4e88ca7cad22
Todd Margolis <todd.margolis@qlik.com>
2026-02-04 15:10:24 -0600

    Fix image filename case for CRCA project in projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -946,7 +946,7 @@ export const projects: Project[] = [
     dimensions: 'University of California, San Diego',
     heroImage: '/images/placeholder.png',
     description: 'Provided organizational administration, research development, project management and grant writing for the Center for Research in Computing and the Arts. Created the design, specification, installation, configuration, operation, training and maintenance routine of CRCA\'s new 10,000 sq ft facilities including Virtual Reality, Spatialized Audio, Motion Capture, Gaming and Digital Cinema labs. Technical Director for dozens of interdisciplinary research projects that have been published and exhibited world-wide, as well as new products that have been brought to market. Managed small to medium groups on short-term projects as well as long-term teams.',
-    images: ['/images/CRCA.jpg']
+    images: ['/images/crca.jpg']
   },
   // 4. Rensselaer Polytechnic Institute
   {
